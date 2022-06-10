6733ed245b297f41dabc408ff506a3bfa112b19f
vbth7777 <vbth7777@gmail.com>
2022-06-10 12:54:11 +0700

    Update 6/10/2022

---
+++ XComicTools/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using QuanLyKho.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XComicTools.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+    }
+}
